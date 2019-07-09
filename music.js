89699dc800e92c28756fe2f41095a2d08af5cb1c
mobyrne9 <meadhbh.obyrne1@ctyi.org>
2019-07-09 14:45:39 +0100

    created gallery

--- music.js
+++ music.js
@@ -1,5 +1,4 @@
-var list = ["For Tomorrow - Blur", "Menial - Mt Eddy", "Fluorescent Adolescent - Arctic Monkeys", "Hostiles - Damon Albarn", "No Surprises - Radiohead", "ZIPPER - Brockhampton"];
-
+var list = ["For Tomorrow - Blur", "Menial - Mt Eddy", "Fluorescent Adolescent - Arctic Monkeys", "Hostiles - Damon Albarn", "No Surprises - Radiohead", "ZIPPER - Brockhampton", "Bethlehem - Declan McKenna", "Dancing In The Moonlight - Thin Lizzy", "Alright - Supergrass", "I Want To Break Free - Queen", "Rebel Rebel - David Bowie", "Changes - David Bowie", "End of a Century - Blur", "Coffee and TV - Blur", "In A Week - Hozier"];
 function buttonClicked() {
   document.getElementById("displayme").innerHTML = 
     list[Math.floor(Math.random()*list.length)];
